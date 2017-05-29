7c26778af0239b9dec8d6f90aa8dc0afa991f854
IvanSliusar <ivan.sliusar.ua@gmail.com>
2017-05-29 16:19:39 +0300

    task 4087

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,14 @@
 package ru.job4j;
+/**
+*Class Calculate решение задачи части 001 урок 1.
+*@autor i.sliusar
+*29.05.2017
+*/
 
 public class Calculate{
+	/**
+	*Тестовый вывод на экран
+	*/
 	public static void main(String[] args){
 		System.out.println("Hello world");
 	}
