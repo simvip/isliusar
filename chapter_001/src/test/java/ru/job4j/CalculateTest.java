f700b842b5645d10c15e4d4c1e780b863fcaf40c
IvanSliusar <ivan.sliusar.ua@gmail.com>
2017-05-30 07:38:46 +0300

    task 4122 JUnit

---
+++ chapter_001/src/test/java/ru/job4j/CalculateTest.java
@@ -0,0 +1,36 @@
+package ru.job4j;
+
+import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+* Test.
+*
+* @author Ivan Sliusar (mailto:ivan.sliusar.ua@gmail.com)
+* @version $Id$
+* @since 0.1
+*/
+public class CalculateTest {
+	/**
+	* Test add.
+	*/
+	@Test
+	public void whenAddOneToOneThenTwo() {
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(out));
+		Calculate.main(null);
+		assertThat(
+				out.toString(),
+				is(
+						String.format(
+								"Hello world%s",
+								System.getProperty("line.separator")
+						)
+				)
+		);
+	}
+}
+ 
