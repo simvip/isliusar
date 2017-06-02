7e0d347ff88d261d7b04b4277f75d18f147eceb2
IvanSliusar <ivan.sliusar.ua@gmail.com>
2017-06-02 16:27:16 +0300

    2.3. Simple Calculator

---
+++ chapter_001/src/main/java/ru/job4j/Calculator/Calculator.java
@@ -0,0 +1,55 @@
+package ru.job4j.calculator;
+
+/**
+ *Class Calculator.
+ *
+ *@author i.sliusar
+ *@version $Id$
+ *@since 0.1
+ *29.05.2017
+ */
+public class Calculator {
+	/**
+	 *Локальная переменная для хранения результата.
+	 */
+    private double result;
+    /**
+	 *add производит сложение двух чисел.
+	 *@param first - Первый аргумент.
+	 *@param second - Второй аргумент.
+	 */
+    public void add(double first, double second) {
+        this.result = first + second;
+    }
+	/**
+	 *Subtraction производит вычитание двух чисел.
+	 *@param first - Первый аргумент.
+	 *@param second - Второй аргумент.
+	 */
+    public void subtraction(double first, double second) {
+        this.result = first - second;
+    }
+	/**
+	 *Subtraction производит вычитание двух чисел.
+	 *@param first - Первый аргумент.
+	 *@param second - Второй аргумент.
+	 */
+    public void multiplication(double first, double second) {
+        this.result = first * second;
+    }
+	/**
+	 *Subtraction производит вычитание двух чисел.
+	 *@param first - Первый аргумент.
+	 *@param second - Второй аргумент.
+	 */
+    public void division(double first, double second) {
+		this.result = first / second;
+	}
+	/**
+	 *Возвращает результата.
+	 *@return возвращает тип double.
+	 */
+    public double getResult() {
+        return this.result;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/Calculator/CalculatorTest.java
@@ -0,0 +1,61 @@
+package ru.job4j.calculator;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ *Class Calculator решение задачи части 001 урок 1.
+ *
+ *@author i.sliusar
+ *@version $Id$
+ *@since 0.1
+ *29.05.2017
+ */
+public class CalculatorTest {
+	/**
+	 *Тест add.
+	 */
+    @Test
+    public void whenAddOnePlusOneThenTwo() {
+        Calculator calc = new Calculator();
+        calc.add(1D, 1D);
+        double result = calc.getResult();
+        double expected = 2D;
+        assertThat(result, is(expected));
+    }
+	/**
+	 *Тест subtraction.
+	 */
+    @Test
+    public void whenSubtractionOneSubOneThenZero() {
+        Calculator calc = new Calculator();
+        calc.subtraction(1D, 1D);
+        double result = calc.getResult();
+        double expected = 0D;
+        assertThat(result, is(expected));
+    }
+	/**
+	 *Тест Multiplication.
+	 */
+    @Test
+    public void whenMultiplicationTwoMultTwoThenFoth() {
+        Calculator calc = new Calculator();
+        calc.multiplication(2D, 2D);
+        double result = calc.getResult();
+        double expected = 4D;
+        assertThat(result, is(expected));
+    }
+	/**
+	 *Тест Division 4/2.
+	 */
+    @Test
+    public void whenDivisionFourDivTwoThenTwo() {
+        Calculator calc = new Calculator();
+        calc.division(4D, 2D);
+        double result = calc.getResult();
+        double expected = 2D;
+        assertThat(result, is(expected));
+    }
+}
