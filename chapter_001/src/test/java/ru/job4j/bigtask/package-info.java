d96c0fa64cb43ce4be18e02d3e530e27e36a41af
IvanSliusar <ivan.sliusar.ua@gmail.com>
2017-06-23 17:50:45 +0300

    2. Приложение должно собираться в jar и запускаться командой java –jar tracker.jar + Фикс п.1

--- chapter_001/src/test/java/ru/job4j/bigtask/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
-* Package for calculate task.
-*
-* @author Ivan Sliusar (mailto:ivan.sliusar.ua@gmail.com)
-* @version $Id$
-* @since 0.1
-*/
-package ru.job4j.bigtask;
--- chapter_002/src/main/java/ru/job4j/start/package-info.java
+++ chapter_002/src/main/java/ru/job4j/start/package-info.java
@@ -5,4 +5,4 @@
 * @version $Id$
 * @since 0.1
 */
-package ru.job4j.bigtask;
+package ru.job4j.start;
