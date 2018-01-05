47593b7160a493bd2369d78f41b9a0c08caadb31
IvanSliusar <ivan.sliusar.ua@gmail.com>
2018-01-05 10:26:29 +0200

    XML XSLT JDBC Оптимизация [#20459]

---
+++ chapter_006_servlet_JSP/src/main/java/servlets/EchoServlet.java
@@ -0,0 +1,28 @@
+package servlets;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+/**
+ * Created by Ivan Sliusar on 03.01.2018.
+ * Red Line Soft corp.
+ */
+public class EchoServlet extends HttpServlet {
+    private static final Logger LOGGER = LoggerFactory.getLogger(EchoServlet.class);
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter writer = new PrintWriter(resp.getOutputStream());
+        writer.append("Hello world");
+        writer.flush();
+    }
+}
