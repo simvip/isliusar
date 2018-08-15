1abf8b5b279a6e3e73a1c2a3e916acbb48a02e79
IvanSliusar <ivan.sliusar.ua@gmail.com>
2018-08-15 17:11:59 +0300

    Add populate db

---
+++ chapter_009_Spring_MVC/src/main/resources/import.sql
@@ -0,0 +1,20 @@
+Insert into users (id,name,email) Values(3,'testUser','test@gmail.com');
+
+Insert into transmission (name) Values('КПП-6');
+Insert into transmission (name) Values('AКПП');
+Insert into transmission (name) Values('Robotic');
+Insert into transmission (name) Values('КПП-4');
+
+Insert into Engine (name) Values('V6');
+Insert into Engine (name) Values('V5');
+Insert into Engine (name) Values('V3');
+
+Insert into GearBox (name) Values('BOX1');
+Insert into GearBox (name) Values('BOX2');
+Insert into GearBox (name) Values('BOX3');
+Insert into GearBox (name) Values('BOX4');
+Insert into GearBox (name) Values('BOX5');
+
+Insert into Car (name,id_transmission,id_engine,id_gearbox) Values('Жига 6',1,1,1);
+Insert into Car (name,id_transmission,id_engine,id_gearbox) Values('Ford Mustang',2,2,2);
+Insert into Car (name,id_transmission,id_engine,id_gearbox) Values('VW Golf',3,3,5);
